ad3408302e2a7a6400acc6c8806634fe32de8e2c
Heidi Ghafoor <57541395+heidigha@users.noreply.github.com>
2024-09-21 19:16:27 -0400

    Update homework_4.sql

--- 02_activities/homework/homework_4.sql
+++ 02_activities/homework/homework_4.sql
@@ -17,6 +17,9 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
+SELECT 
+product_name || ', ' || (COALESCE(product_size,'') ) || ' (' || COALESCE(product_qty_type,'unit') || ')'
+FROM product
 
 
 
